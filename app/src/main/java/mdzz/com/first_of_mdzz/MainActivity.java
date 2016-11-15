16adf4fd0feb733fbf935cbd15b6b6c5f7665391
apple <apple@appledeMacBook-Pro-2.local>
2016-11-15 08:43:27 +0800

    Initial commit

---
+++ app/src/main/java/mdzz/com/first_of_mdzz/MainActivity.java
@@ -0,0 +1,13 @@
+package mdzz.com.first_of_mdzz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
